7f7300b0752508c27687c20e9c8ef3e0b25bc264
jcrealreal <jcmaniegowoah@gmail.com>
2026-06-23 20:21:32 +0800

    Add files via upload

---
+++ Mga basic ng java/main.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class main
+{
+	public static void main(String[] args) {
+		Scanner input  = new Scanner(System.in);
+		//Args
+		String user;
+		int id;
+		String color;
+ 
+		//user input
+		System.out.println("Enter your user ");
+		user = input.nextLine();
+ 
+		System.out.println("Enter your userid  ");
+		id = input.nextInt();
+ 
+	    System.out.println("Enter your color  ");
+ 
+	    color  = input.next();
+ 
+	    //loop 
+	    for (int i = 1; i < 4; i++){
+	        System.out.println("Output  #" +i);
+	        System.out.println("Username " +user);
+	         System.out.println("Id  "+id);
+	            System.out.println("Color "+color);
+	    }
+        
+	}
+}
