718dae3e32e1c9dbcc6c495e39182d93578ff7bc
James Clerick <jcmaniegowoah@gmail.com>
2026-07-06 14:43:52 +0800

    Create class(ex).java

---
+++ Mga basic ng java/class(ex).java
@@ -0,0 +1,28 @@
+//eto i iimport lang to  sample lang to
+class person{
+ String first;
+ String last;
+ char sex;
+ int age;
+}
+
+
+//main class (dito mag fufunction lahat )
+public class Main
+{
+	public static void main(String[] args) {
+ 
+    person pe = new person();
+    pe.first  ="Bert";
+    pe.last = "ewan ";
+    pe.sex = 'M';
+    pe.age = 67;
+    System.out.println(pe.first);
+    System.out.println(pe.last);
+    System.out.println(pe.sex);
+    System.out.println(pe.age);
+	}
+}
+
+
+
