ab561e2320d111028faa683437d849d0f83d30bb
James Clerick <jcmaniegowoah@gmail.com>
2026-07-06 15:14:43 +0800

    Create vehicle.java

---
+++ Mga basic ng java/vehicle.java
@@ -0,0 +1,23 @@
+ class vehicle{
+ String vehicle;
+ String plate;
+ String engine;
+}
+
+//main class (dito mag fufunction lahat )
+public class vehicle
+{
+	public static void main(String[] args) {
+	    vehicle c = new vehicle();
+    c.vehicle  =  "hiace";
+    c.plate  = "ABC 1234";
+    c.engine  = "2KD-FTV";
+    
+    System.out.println(c.vehicle);
+     System.out.println(c.plate);
+     System.out.println(c.engine);
+}
+}
+
+
+
